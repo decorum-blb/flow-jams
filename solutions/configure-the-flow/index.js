fdfffdde666f64b751fefb0c25e8352284600fa8
Adam Edwards <adamcharlesedwards@gmail.com>
2018-08-02 11:45:28 -0500

    feature(configure-the-flow): add configure-the-flow steps and solution

--- solutions/configure-the-flow/index.js
+++ solutions/configure-the-flow/index.js
@@ -1,1 +1,21 @@
-console.log('hello');
+const packageJson = {
+  "name": "go-with-the-flow",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "build": "babel src/ -d lib/",
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "flow": "flow"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "devDependencies": {
+    "babel-cli": "^6.26.0",
+    "babel-preset-flow": "^6.23.0",
+    "flow-bin": "^0.77.0"
+  }
+};
+
+console.log(JSON.stringify(packageJson));
